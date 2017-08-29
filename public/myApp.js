a0e632ceff940b3d2662f38c1b52c0c7014329eb
manish jaiswal <mjaiswal@paypal.com>
2017-08-29 15:36:30 -0500

    Commit 0829-04

--- public/myApp.js
+++ public/myApp.js
@@ -1,6 +1,7 @@
 angular.module('myApp',[])
 .constant('VERSION',1.1)
 .controller('MyCtrl',function (VERSION,$scope) {
-    $scope.version = VERSION;
-    $scope.gender = 'M';
+    var ctrl = this;
+    this.version  = VERSION;
+    this.gender = 'M';
 });
