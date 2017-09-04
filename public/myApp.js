56361103f4d19bb2c898d5f915e0459b2f38d2f2
Manish Jaiswal <manish23@gmail.com>
2017-09-04 12:35:12 -0500

    Commit 0904

--- public/myApp.js
+++ public/myApp.js
@@ -1,7 +1,35 @@
-angular.module('myApp',[])
+angular.module('myApp',['ngMessages'])
 .constant('VERSION',1.1)
 .controller('MyCtrl',function (VERSION,$scope) {
     var ctrl = this;
-    this.version  = VERSION;
-    this.gender = 'M';
+    ctrl.version  = VERSION;
+    ctrl.gender = 'M';
+    ctrl.validForm=false;
+    ctrl.submit = function(form){
+        console.log(form.$valid)
+        if(form.$valid==true)
+        {
+            ctrl.validForm=true;
+        }
+
+
+        // console.log(inputForm.$valid);
+        // console.log($scope.valid)
+    }
+    ctrl.clear = function(){
+        ctrl.validForm=false;
+        ctrl.fname=null;
+        ctrl.dtask=null;
+        ctrl.oceleb=null;
+        ctrl.jtitle=null;
+        ctrl.celeb=null;
+        ctrl.hnum=null;
+        ctrl.ttask=null;
+        ctrl.uskill=null;
+        ctrl.adj=null;
+
+
+        // console.log(inputForm.$valid);
+        // console.log($scope.valid)
+    }
 });
